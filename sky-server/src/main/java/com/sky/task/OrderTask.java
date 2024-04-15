ee5f636bae1db0c3c62a3c81a4d5afdd085de8dc
lyrion <2867671761@qq.com>
2024-04-15 15:45:10 +0800

    来单提醒业务代码开发

--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -20,8 +20,8 @@ public class OrderTask {
     /**
      * 处理超时订单的方法
      */
-    //@Scheduled(cron = "0 * * * * ?")  //每分钟触发一次
-    @Scheduled(cron = "1/5 * * * * ?")
+    @Scheduled(cron = "0 * * * * ?")  //每分钟触发一次
+    //@Scheduled(cron = "1/5 * * * * ?")
     public void processTimeoutOrder(){
         log.info("定时处理超时订单: {}", LocalDateTime.now());
 
@@ -41,8 +41,8 @@ public class OrderTask {
     /**
      * 定时处理一直在派送中的订单
      */
-    //@Scheduled(cron = "0 0 1 * * ?")
-    @Scheduled(cron = "0/5 * * * * ?")
+    @Scheduled(cron = "0 0 1 * * ?")
+    //@Scheduled(cron = "0/5 * * * * ?")
     public void processDeliveryOrder(){
         log.info("定时处理一直在派送中的订单：{}", LocalDateTime.now());
 
